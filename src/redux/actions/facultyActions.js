29daa4b36860e1caebada592460c0fe4536bfae8
harada1031 <olavijokela951@outlook.com>
2024-10-06 08:48:10 +0900

    mainly works

---
+++ src/redux/actions/facultyActions.js
@@ -0,0 +1,123 @@
+import {
+  SET_ERRORS,
+  FACULTY_LOGIN,
+  FACULTY_REGISTER,
+  UPDATE_PASSWORD,
+  UPDATE_FACULTY,
+  ADD_TEST,
+  GET_TEST,
+  GET_STUDENT,
+  MARKS_UPLOADED,
+  ATTENDANCE_MARKED,
+} from "../actionTypes";
+import * as api from "../api";
+
+export const facultySignIn = (formData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.facultySignIn(formData);
+    console.log(data);
+    dispatch({ type: FACULTY_LOGIN, data });
+    if (data.result.email) navigate("/faculty/home");
+    else navigate("/faculty/password");
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const facultySignUp = (formData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.facultySignUp(formData);
+    dispatch({ type: FACULTY_REGISTER, data });
+    if (data.result) navigate("/login/facultylogin");
+    else navigate("/faculty/password");
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const facultyUpdatePassword =
+  (formData, navigate) => async (dispatch) => {
+    try {
+      const { data } = await api.facultyUpdatePassword(formData);
+      dispatch({ type: UPDATE_PASSWORD, payload: true });
+      alert("Password Updated");
+      navigate("/faculty/home");
+    } catch (error) {
+      dispatch({ type: SET_ERRORS, payload: error.response.data });
+    }
+  };
+
+export const updateFaculty = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.updateFaculty(formData);
+    dispatch({ type: UPDATE_FACULTY, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const createTest = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.createTest(formData);
+    alert("Test Created Successfully");
+
+    dispatch({ type: ADD_TEST, payload: true });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const getTest = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.getTest(formData);
+    dispatch({ type: GET_TEST, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const getStudent = (formData) => async (dispatch) => {
+  try {
+    const { data } = await api.getMarksStudent(formData);
+    dispatch({ type: GET_STUDENT, payload: data });
+  } catch (error) {
+    dispatch({ type: SET_ERRORS, payload: error.response.data });
+  }
+};
+
+export const uploadMark =
+  (marks, department, section, year, test) => async (dispatch) => {
+    try {
+      const formData = {
+        marks,
+        department,
+        section,
+        year,
+        test,
+      };
+      const { data } = await api.uploadMarks(formData);
+      alert("Marks Uploaded Successfully");
+      dispatch({ type: MARKS_UPLOADED, payload: true });
+    } catch (error) {
+      dispatch({ type: SET_ERRORS, payload: error.response.data });
+    }
+  };
+
+export const markAttendance =
+  (checkedValue, subjectName, department, year, section) =>
+  async (dispatch) => {
+    try {
+      const formData = {
+        selectedStudents: checkedValue,
+        subjectName,
+        department,
+        year,
+        section,
+      };
+      const { data } = await api.markAttendance(formData);
+      alert("Attendance Marked Successfully");
+      dispatch({ type: ATTENDANCE_MARKED, payload: true });
+    } catch (error) {
+      dispatch({ type: SET_ERRORS, payload: error.response.data });
+    }
+  };
